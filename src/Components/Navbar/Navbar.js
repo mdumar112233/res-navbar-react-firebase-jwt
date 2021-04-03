4f1f2b9704064e7f515a2420c76a18b4afc5bb44
md umar faruk <mdumar11233>
2021-04-03 23:38:32 +0600

    added new code

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,7 +4,14 @@ import '../../App.css';
 const Navbar = () => {
     return (
         <div className='Navbar'>
-            <div className="leftSide"></div>
+            <div className="leftSide">
+                <div className="links">
+                    <a href="">Home</a>
+                    <a href="">Feddback</a>
+                    <a href="">About us</a>
+                    <a href="">Contact us</a>
+                </div>
+            </div>
             <div className="rightSide"></div>
         </div>
     );
