54844e102cd892ba6a075a5dc978c5722666274f
md umar faruk <mdumar11233>
2021-04-03 15:33:57 +0600

    navbar with react firebase jwt

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+
+const Navbar = () => {
+    return (
+        <div className='Navbar'>
+            <div className="leftSide"></div>
+            <div className="rightSide"></div>
+        </div>
+    );
+};
+
+export default Navbar;
