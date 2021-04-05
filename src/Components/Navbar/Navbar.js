100231a6d007fc5a1f6168bb52e41a04fe96bca5
md umar faruk <mdumar11233>
2021-04-05 08:40:08 +0600

    added new code

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../App.css';
 
 const Navbar = () => {
+    const [showLinks , setShowLinks] = useState(false);
+
     return (
         <div className='Navbar'>
             <div className="leftSide">
-                <div className="links">
+                <div className="links" id={showLinks ? 'hidden' : ''}>
                     <a href="">Home</a>
                     <a href="">Feddback</a>
                     <a href="">About us</a>
                     <a href="">Contact us</a>
                 </div>
+                <button onClick={() => setShowLinks(!showLinks)}>open</button>
             </div>
             <div className="rightSide">
                 <input type="text" placeholder='search here'/>
